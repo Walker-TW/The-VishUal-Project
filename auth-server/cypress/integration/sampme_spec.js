a3c827063475aab80d5c43c7b8cee34d69dad59c
Lucian Boatright <lucian.boatright@gmail.com>
2020-03-04 12:37:17 +0000

    added cypress test desription to sample test

--- auth-server/cypress/integration/sampme_spec.js
+++ auth-server/cypress/integration/sampme_spec.js
@@ -1,5 +1,12 @@
-describe('my First test', function(){
-  it('Tests frame', function(){
+describe('my First test', function() {
+  it('Tests frame', function() {
+    //arrange = setup initial app state
+    // - visit a web page
+    // - query for element
+    // Act - take an action
+    // - interact with that element
+    // Assert - make an assertion
+    // - make an assertion about page content 
     expect(true).to.equal(true)
   })
-})
+}) 
