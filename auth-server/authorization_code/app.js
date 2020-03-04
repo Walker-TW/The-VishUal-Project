5693ae736b77fa90ab4a1154f8048e10f8bb6d97
Walker-TW <thomas.w.walker@live.co.uk>
2020-03-04 15:46:07 +0000

    Changed request.post to force redirect

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -130,9 +130,8 @@ app.get('/refresh_token', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
+      let uri = process.env.REDIRECT || 'http://localhost:3000'
+      res.redirect(uri + '?access_token=' + access_token)
     }
   });
 });
